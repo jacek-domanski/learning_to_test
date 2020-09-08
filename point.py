db3a4c97a2751292b67565773c810a08f302872e
jacek-domanski <j.domanski92@gmail.com>
2020-09-08 18:08:39 +0200

    :sparkles: Add point class

---
+++ point.py
@@ -0,0 +1,18 @@
+from math import sqrt
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __sub__(self, other):
+        dist_x = other.x - self.x
+        dist_y = other.y - self.y
+
+        return sqrt(pow(dist_x, 2) + pow(dist_y, 2))
+
+if __name__ == '__main__':
+    pt_a = Point(0, 0)
+    pt_b = Point(3, 4)
+    print(pt_a-pt_b)
