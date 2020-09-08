ae5048bf530ff32baf24fdd1a63ae1af6e6c7e53
jacek-domanski <j.domanski92@gmail.com>
2020-09-08 19:22:20 +0200

    :sparkles: Add EQ and NE magic methods to point class

--- point.py
+++ point.py
@@ -12,6 +12,12 @@ class Point:
 
         return sqrt(pow(dist_x, 2) + pow(dist_y, 2))
 
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return self.x != other.x or self.y != other.y
+
 if __name__ == '__main__':
     pt_a = Point(0, 0)
     pt_b = Point(3, 4)
